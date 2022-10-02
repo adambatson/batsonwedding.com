4a457f80ab93756d8e6ab86191b963d47c950eae
Adam Batson <adamjbatson@gmail.com>
2022-10-02 19:57:09 -0400

    Funky fresh animations on mobile

--- assets/ts/app/navbar.ts
+++ assets/ts/app/navbar.ts
@@ -3,23 +3,34 @@ import { safeGetElementById } from './utils';
 export class NavBar {
     private _mobileNav: HTMLElement;
     private _mobileNavewrapper: HTMLElement;
+    private _mobileNavToggle: HTMLElement;
 
     public constructor() {
         this._mobileNav = safeGetElementById('mobile-nav');
         this._mobileNavewrapper = safeGetElementById('mobile-nav-wrapper');
+        this._mobileNavToggle = safeGetElementById('mobile-nav-toggle');
 
         this._mobileNav.onclick = () => {
             this.navBarClick();
+        };
+
+        this._mobileNavToggle.onclick = () => {
+            this.toggleHamburger();
         };
     }
 
     private navBarClick(): void {
         if (this._mobileNavewrapper.classList.contains('hidden')) {
             this._mobileNavewrapper.classList.remove('hidden');
-        } else if (this._mobileNavewrapper.classList.contains('hidden-animate')) {
-            this._mobileNavewrapper.classList.remove('hidden-animate');
         } else {
-            this._mobileNavewrapper.classList.add('hidden-animate');
+            this._mobileNavewrapper.classList.toggle('hidden-animate');
         }
+    }
+
+    private toggleHamburger(): void {
+        var lines = this._mobileNavToggle.querySelectorAll('.line');
+        lines.forEach((el) => {
+            el.classList.toggle('change');
+        });
     }
 }

83058a5d39cbee8909358ab19eb8f663d90868d6
Adam J Batson <adamjbatson@gmail.com>
2022-10-05 20:02:01 -0400

    First pass cleanin up old sass

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
+        'postcss-mixins': {},
         'postcss-simple-vars': {},
+        'postcss-nested': {},
     },
 };

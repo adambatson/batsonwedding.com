e80bbeb1708b02c132cee2eb603d9f9243fbd483
Adam J Batson <adamjbatson@gmail.com>
2023-01-18 17:52:56 -0500

    Add post css presets

--- postcss.config.ts
+++ postcss.config.ts
@@ -3,6 +3,7 @@ module.exports = {
         'tailwindcss/nesting': {},
         tailwindcss: { config: './tailwind.config.ts' },
         autoprefixer: {},
+        'postcss-preset-env': {},
         'postcss-mixins': {},
         'postcss-simple-vars': {},
         'postcss-nested': {},
